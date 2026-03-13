d43f5d1dd10acca3c80e8a1ad7ec71caee6323d5
jeddy3 <jeddy3@users.noreply.github.com>
2026-03-13 19:51:11 +0000

    Use stylelint-config-modern

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,21 +1,14 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard", "stylelint-config-alphabetical-order"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-modern",
+    "stylelint-config-alphabetical-order",
+  ],
   overrides: [
     {
       files: ["**/*.js"],
       customSyntax: "postcss-lit",
     },
   ],
-  rules: {
-    "declaration-property-value-no-unknown": [
-      true,
-      {
-        ignoreProperties: {
-          "clip-path": /.*/,
-          cursor: /.*/,
-        },
-      },
-    ],
-  },
 };
