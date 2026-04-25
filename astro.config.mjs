e204044e09359bbc83b8ae9b0d61588de553c01d
Richard Hallows <jeddy3@users.noreply.github.com>
2026-04-25 15:11:13 +0100

    Add spotlight for local dev errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import spotlight from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [spotlight()],
   markdown: {
     shikiConfig: {
       themes: {
